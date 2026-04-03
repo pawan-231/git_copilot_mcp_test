6cefffd93bdc71c70b1c372cffea13fc08dddb08
Developer <dev@example.com>
2026-04-03 17:25:45 +0530

    Initial commit: Spring Boot project

---
+++ src/main/java/com/gitcopilotspringtest/GitcopilotspringtestApplication.java
@@ -0,0 +1,13 @@
+package com.gitcopilotspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcopilotspringtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitcopilotspringtestApplication.class, args);
+    }
+
+}
